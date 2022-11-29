b0d86b45e2a9ad6acfd01b601ea79be2909ce39c
Tim.M <mazzolinitim@gmail.com>
2022-11-29 12:04:20 +0100

    Main_Window_Avancement

--- Onzzer/Onzzer.py
+++ Onzzer/Onzzer.py
@@ -1,10 +1,11 @@
 import sys 
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QFormLayout, QGridLayout
+import os
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QFormLayout, QGridLayout, QPushButton
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QMenu, QAction, QToolBar, QDockWidget
 from PyQt5.QtWidgets import QLabel, QLineEdit, QTextEdit, QGroupBox
 from PyQt5.QtWidgets import QMessageBox, QInputDialog
 
-from PyQt5.QtGui import QCursor, QIcon, QPixmap, QKeySequence, QFileDialog 
+from PyQt5.QtGui import QCursor, QIcon, QPixmap, QKeySequence, QPixmap
 from PyQt5.QtCore import Qt, QDir
 
 
@@ -15,40 +16,25 @@ class MaPremiereAppli(QMainWindow):
         super().__init__()
         self.setWindowTitle("Onzzer")
         self.setWindowIcon(QIcon('Icones/logo.png'))
-        self.resize(1000,800)
+        #self.setStyleSheet("background-color: Blue;")
+        self.resize(500,500)
         self.menu()
-
+        self.central()
         
     
     def menu(self):
         eraseButton = QAction(QIcon('Icones/edit-undo.png'), 'Effacer', self)
         eraseButton.setShortcut('Ctrl+N')
-        eraseButton.setStatusTip('Ouvrir un fichier')
-
         openButton = QAction(QIcon('Icones/mail-send.png'), "Ouvrir l'emplacement d'enregistrement", self)
         openButton.setShortcut('Ctrl+O')
-        openButton.setStatusTip("")
-        
         exitButton = QAction(QIcon('Icones/application-exit.png'), 'Quitter', self)
         exitButton.setShortcut('Ctrl+Q')
-        exitButton.setStatusTip('Exit application')
-
-
-        serverButton = QAction('Serveur SMTP', self)
-        serverButton.setShortcut('Ctrl+X')
-        
-        expButton = QAction('Expéditeur', self)
-        expButton.setShortcut('Ctrl+C')
-        
-        signButton = QAction('Signature', self)
-        signButton.setShortcut('Ctrl+V')
-        
         manButton = QAction('A Propos', self)
         manButton.setShortcut('F1')
-        manButton.triggered.connect(self.action_a_propos)
         
-
         exitButton.triggered.connect(self.close)
+        openButton.triggered.connect(self.action_openfolder)
+        manButton.triggered.connect(self.action_a_propos)
 
         menu = self.menuBar()
         menufichier = menu.addMenu("&Fichier")
@@ -56,26 +42,48 @@ class MaPremiereAppli(QMainWindow):
         menufichier.addAction(openButton)
         menufichier.addAction(exitButton)
         
-        menuedition = menu.addMenu("&Parametres")
-        menuedition.addAction(serverButton)
-        menuedition.addAction(expButton)
-        menuedition.addAction(signButton)
-        
         menuAide = menu.addMenu("&Aide")
         menuAide.addAction(manButton)
                
         toolbar = QToolBar("Ma barre d'outils")
         self.addToolBar(toolbar)
         toolbar.addAction(eraseButton)
-        toolbar.addAction(openButton)
+        toolbar.addAction(exitButton)
+
+
+
+    def central(self):
+        
+        wid_onzzer = QWidget()
+        grid_box = QGridLayout()
+        box_image = QLabel()
+        image = QPixmap('Icones/logo_long.png')
+        wid_search = QWidget()
+        line = QLineEdit()
+        searchButton = QPushButton("Recherche")
+        self.setCentralWidget(wid_onzzer)
+
+        wid_onzzer.setLayout(grid_box)
+        box_image.setPixmap(image)
+        
+        grid_box.addWidget(box_image, 0, 2)
+        grid_box.addWidget(line, 1, 1, 1, 3)
+        grid_box.addWidget(searchButton, 1, 4)
+        grid_box.setVerticalSpacing(2)
+        grid_box.setContentsMargins(200, 200, 200, 400)
+
+        #wid_onzzer.setStyleSheet("background-color: Blue;")
+        line.setStyleSheet("background-color: white;")
+        searchButton.setStyleSheet("background-color: white; border-style: outset; border-width: 1px;")
+
+
+
+
 
 
 
     def action_openfolder(self) :
-        fichier = QFileDialog.getOpenFileName(self, 
-                     "Ouvrir un fichier", 
-                     "/cheminverslefichier", 
-                     "JPEG (*.jpg *.jpeg);; TIFF (*.tif);; All files (*.*)")
+        os.system("%SystemRoot%\explorer.exe /n,/e, monurl")
        
 
 
@@ -83,8 +91,7 @@ class MaPremiereAppli(QMainWindow):
 
 
     def action_a_propos(self):
-            
-        QMessageBox.information(self,"Onzzer Application de Recherche Musicale", "onzzer par Baptiste Tarte, Tim Mazzolini, Eliot Monneau, Matthieu Brissonnet")
+        QMessageBox.information(self,"Onzzer Application de Recherche Musicale", "Onzzer par Baptiste Tarte, Tim Mazzolini, Eliot Monneau, Matthieu Brissonnet")
 
 
 def main():
