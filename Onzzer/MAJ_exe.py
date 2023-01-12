ece6be7895a767bdcec71cd0433db1d1e002f6ad
Tim.M <mazzolinitim@gmail.com>
2023-01-12 22:47:04 +0100

    Onzzer 3.0.6

--- Onzzer/MAJ_exe.py
+++ Onzzer/MAJ_exe.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # This program is used to create a new version of the Onzzer.exe App
-
+os.chdir("./Onzzer")
 command = 'pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/Icones/app.ico" --add-data "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/Icones;Icones/" --add-data "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/request_albums.py;." --add-data "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/request_artistes.py;." --add-data "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/request_pistes.py;." --add-data "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/youtube_search.py;." --hidden-import "json" --hidden-import "requests" --hidden-import "PyQt5" --hidden-import "os" --hidden-import "sys" --hidden-import "webbrowser"  "C:/Users/Tim/source/repos/Azertim17/Onzzer/Onzzer/Onzzer.py"'
 os.system(command)
 os.replace("./dist/Onzzer.exe","./Onzzer.exe")
